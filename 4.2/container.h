d362737fe799352807f06dd02aec28b0f34cf226
DrLafa <godfuzz0r1337@gmail.com>
2021-02-09 21:27:30 +0300

    container class with iterator

--- 4.2/container.h
+++ 4.2/container.h
@@ -4,21 +4,173 @@
 #include <utility>
 #include <iostream>
 namespace oop4 {
-
   template <typename T1, typename T2>
   struct T{
       T1 first;
       T2 second;
   };
 
-  template<typename T1, typename T2>
+  template <class T1, class T2>
+	class MapIterator : public std::iterator<std::random_access_iterator_tag, T<T1,T2>> {
+	private:
+		T<T1,T2>* buf;
+	public:
+		MapIterator(T<T1,T2>* c) : buf(c) {}
+
+		bool operator =(MapIterator<T1,T2> it);
+		bool operator ==(const MapIterator<T1,T2> it) const;
+		bool operator !=(const MapIterator<T1,T2> it) const;
+		MapIterator<T1,T2>& operator ++();
+		MapIterator<T1,T2> operator ++(int);
+		MapIterator<T1,T2>& operator --();//pfiks
+		MapIterator<T1,T2> operator --(int);
+		T<T1,T2>& operator *();
+		T<T1,T2>* operator ->();
+		MapIterator<T1,T2>& operator +=(int n);
+		MapIterator<T1,T2> operator +(int n);
+		MapIterator<T1,T2>& operator -=(int n);
+		MapIterator<T1,T2> operator -(int n);
+		int operator -(MapIterator<T1,T2> it);
+		T<T1,T2>& operator [](int n);
+		bool operator <(MapIterator<T1,T2> it);
+		bool operator >(MapIterator<T1,T2> it);
+		bool operator <=(MapIterator<T1,T2> it);
+		bool operator >=(MapIterator<T1,T2> it);
+	};
+
+	template <typename T1, typename T2>
+	MapIterator<T1,T2>& MapIterator<T1,T2>::operator++() {
+		++buf;
+		return *this;
+	}
+
+	template <typename T1, typename T2>
+	MapIterator<T1,T2> MapIterator<T1,T2>::operator++(int) {
+		MapIterator<T1,T2> tmp = buf;
+		++buf;
+		return tmp;
+	}
+
+	template <typename T1, typename T2>
+	MapIterator<T1,T2>& MapIterator<T1,T2>::operator --() {
+		--buf;
+		return *this;
+	}
+
+	template <typename T1, typename T2>
+	MapIterator<T1,T2> MapIterator<T1,T2>::operator --(int) {
+		MapIterator<T1,T2> tmp = buf;
+		--buf;
+		return tmp;
+	}
+
+	template <typename T1, typename T2>
+	T<T1,T2>& MapIterator<T1,T2>::operator *() {
+		return *buf;
+	}
+
+	template <typename T1, typename T2>
+	T<T1,T2>* MapIterator<T1,T2>::operator ->() {
+		return buf;
+	}
+
+	template <typename T1, typename T2>
+	bool MapIterator<T1,T2>::operator ==(const MapIterator<T1,T2> it) const {
+		return buf == it.buf;
+	}
+
+	template <typename T1, typename T2>
+	bool MapIterator<T1,T2>::operator !=(const MapIterator<T1,T2> it) const {
+		return buf != it.buf;
+	}
+
+	template <typename T1, typename T2>
+	MapIterator<T1,T2>& MapIterator<T1,T2>::operator +=(int n) {
+		if (n > 0) {
+			while (n > 0) {
+				n--;
+				++(*this);
+			}
+		}
+		else {
+			while (n < 0) {
+				n++;
+				--(*this);
+			}
+		}
+		return(*this);
+	}
+
+	template <typename T1, typename T2>
+	MapIterator<T1,T2> MapIterator<T1,T2>::operator +(int n) {
+		MapIterator<T1,T2> tmp = *this;
+		return tmp += n;
+	}
+
+	template <typename T1, typename T2>
+	MapIterator<T1,T2>& MapIterator<T1,T2>::operator -=(int n) {
+		return (*this += -n);
+	}
+
+	template <typename T1, typename T2>
+	MapIterator<T1,T2> MapIterator<T1,T2>::operator -(int n) {
+		MapIterator<T1,T2> tmp = *this;
+		return tmp -= n;
+	}
+
+	template <typename T1, typename T2>
+	int MapIterator<T1,T2>::operator -(MapIterator<T1,T2> it) {
+		int counter = 0;
+		if (buf <= it.buf) {
+			while ((*this - counter) != it) counter--;
+		}
+		else {
+			while ((*this - counter) != it) counter++;
+		}
+		return counter;
+	}
+
+	template <typename T1, typename T2>
+	T<T1,T2>& MapIterator<T1,T2>::operator [](int n) {
+		return (*(*this + n));
+	}
+
+	template <typename T1, typename T2>
+	bool MapIterator<T1,T2>::operator < (MapIterator<T1,T2> it) {
+		if (it - *this > 0) return true;
+		else return false;
+	}
+
+	template <typename T1, typename T2>
+	bool MapIterator<T1,T2>::operator >(MapIterator<T1,T2> it) {
+		return it < *this;
+	}
+
+	template <typename T1, typename T2>
+	bool MapIterator<T1,T2>::operator <=(MapIterator<T1,T2> it) {
+		return !(*this > it);
+	}
+
+	template <typename T1, typename T2>
+	bool MapIterator<T1,T2>::operator >=(MapIterator<T1,T2> it) {
+		return !(*this < it);
+	}
+
+	template <typename T1, typename T2>
+	bool MapIterator<T1,T2>::operator =(MapIterator<T1,T2> it) {
+		buf = it.buf;
+		return true;
+	}
+
+
+  template<class T1, class T2>
   class map{
   private:
       int size;
-      T<T1, T2>* buff;
+      T<T1, T2> * buff;
 
   public:
-      typedef T<T1, T2> Iterator;
+      typedef MapIterator<T1, T2> Iterator;
 
       map(): size(0), buff(nullptr){}
 
@@ -26,34 +178,33 @@ namespace oop4 {
         delete[] buff;
       }
 
+      Iterator begin() {
+  			return Iterator(this->buff);
+  		}
+  		Iterator end() {
+  			return Iterator(this->buff + this->size);
+  		}
+
       void remove(){
           delete[] buff;
           buff = nullptr;
           size = 0;
       }
 
-      Iterator* begin(){
-          return this->buff;
-      }
-
-      Iterator* end(){
-          return (this->buff + this->size);
-      }
-
-      Iterator* find(T1 key){
-          for (Iterator* i = begin(); i != end(); i++){
+      Iterator find(T1 key){
+          for (Iterator i = begin(); i != end(); i++){
               if (i->first == key)
                   return i;
           }
           return nullptr;
       }
 
-      std::pair<Iterator*, bool> insert(std::pair<T1, T2> elem){
-          if (find(elem.first)){
-              return std::pair<Iterator*, bool>(nullptr, false); //найден
+      Iterator insert(std::pair<T1, T2> elem){
+          if (find(elem.first) != nullptr){
+              return nullptr; //найден
           }
 
-          Iterator * tmp_buf = new Iterator[size+1];
+          T<T1, T2> * tmp_buf = new T<T1, T2>[size+1];
           for(int i = 0; i < size; i++){
             tmp_buf[i].first = buff[i].first;
             tmp_buf[i].second = buff[i].second;
@@ -65,23 +216,28 @@ namespace oop4 {
           buff = tmp_buf;
 
           size++;
-          return std::pair<Iterator*, bool>((this->buff + size-1), true);
+          return Iterator(this->buff);
       }
 
-      bool erase(Iterator* elem){
-          Iterator * tmp_buf = new Iterator[size-1];
-
-          if (find(elem->first) == nullptr){
+      bool erase(T1 key){
+          if (size == 0)
             return false;
-          }
-          for (int i,j = 0; i < size; i++){
-            if ((buff[j].first != elem->first) && (buff[j].second != elem->second)){
-              tmp_buf[i] = buff[j];
-            }else{
-              j++;
+          if (find(key) == nullptr)
+            return false;
+
+          T<T1, T2> * tmp_buf = new T<T1, T2>[size-1];
+          std::cout << "1\n";
+          std::cout << "2\n";
+
+          int i = 0;
+          for (Iterator it = begin(); it != end(); it++){
+            if (it->first != key){
+              tmp_buf[i].first = it->first;
+              tmp_buf[i].second = it->second;
+              i++;
             }
-            j++;
           }
+
           delete[] buff;
           buff = tmp_buf;
           return true;
--- 4.2/test.cpp
+++ 4.2/test.cpp
@@ -1,44 +1,45 @@
 #include "container.h"
 #include <iostream>
+#include <string>
 
 int main(){
-  oop4::map<int, int> test;
+  oop4::map<int, std::string> test;
 
   // добавляем
-  test.insert(std::pair<int, int>(1,1));
-  std::cout << test[1] << "\n";
+  test.insert(std::pair<int, std::string>(1,"kek"));
+
+  // вывод через перегрузку
+  std::cout << "overloaded output: " << test[1] << "\n";
 
   // ищем
   auto it = test.find(1);
-  std::cout << it->second << "\n";
+  std::cout << "search with iterators: " << it->second << "\n";
+
+  // добавим ещё
+  test.insert(std::pair<int, std::string>(2,"asdasdasd"));
+  test.insert(std::pair<int, std::string>(4,"chetire"));
+
+  //выводим всё
+  std::cout << "\n";
+  for (auto it = test.begin(); it != test.end(); it++){
+    std::cout << it->first << ": " << it->second << "\n";
+  }
 
   // удаляем
-  test.erase(it);
+  test.erase(1);
+  std::cout << "Check after remove...\n";
   try{
     std::cout << test[1] << "\n";
   }catch(const char * str){
-    std::cout << "Exception: " << str << std::endl;
+    std::cout << "[+] Exception: " << str << std::endl;
   }
-
-  // добавляем
-  test.insert(std::pair<int, int>(2,2));
-  std::cout << test[2] << "\n";
 
   // удаляем всё
   test.remove();
-  try{
-    std::cout << test[1] << "\n";
-  }catch(const char * str){
-    std::cout << "Exception: " << str << std::endl;
+  std::cout << "\n";
+  for (auto it = test.begin(); it != test.end(); it++){
+    std::cout << it->first << ": " << it->second << "\n";
   }
 
-  std::cout << "[*] Testing assignment\n";
-  test[3]=3;
-  std::cout << "[*] Done\n";
-  try{
-    std::cout << test[3] << "\n";
-  }catch(const char * str){
-    std::cout << "Exception: " << str << std::endl;
-  }
   return 0;
 }
